0893bce8c3e7b7c0f2eca7da3ea6f1bd42f241de
Nathan Lindenbaum <n.lindenbaum93@gmail.com>
2023-01-05 14:19:55 -0500

    ObdDashboard: finishing up layout. Finalize layout on Engine Page

--- src/main/java/Dashboard.java
+++ src/main/java/Dashboard.java
@@ -4,15 +4,30 @@ public class Dashboard {
     private JPanel mainPanel;
     private JTabbedPane mainTabPanel;
     private JPanel basicTab;
-    private JPanel tireTab;
+    private JPanel engineTab;
     private JPanel errorTab;
-    private JPanel miscTab;
+    private JPanel fuelTab;
     private JPanel secondaryPanel;
     private JLabel basicLabel;
-    private JLabel tireLabel;
     private JLabel errorLabel;
-    private JLabel miscLabel;
+    private JLabel fuelLabel;
     private JTable errorCodeTable;
+    private JLabel tempsLabel;
+    private JPanel tempTab;
+    private JPanel hondaTab;
+    private JLabel hondaLabel;
+    private JLabel speedText;
+    private JLabel rpmText;
+    private JLabel massAirFlowText;
+    private JLabel engineLoadText;
+    private JLabel throttlePositionText;
+    private JLabel runtimeText;
+    private JLabel speedLabel;
+    private JLabel rpmLabel;
+    private JLabel massAirFlowLabel;
+    private JLabel engineLoadLabel;
+    private JLabel throttlePositionLabel;
+    private JLabel runtimeLabel;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
