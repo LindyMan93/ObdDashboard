74e16e4f7944f0a19c6b7fc0056127f3abf6b678
Nathan Lindenbaum <n.lindenbaum93@gmail.com>
2023-01-05 10:26:29 -0500

    Initial commit

---
+++ src/main/java/Dashboard.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Dashboard {
+    private JPanel mainPanel;
+    private JTabbedPane mainTabPanel;
+    private JPanel basicTab;
+    private JPanel tireTab;
+    private JPanel errorTab;
+    private JPanel miscTab;
+    private JPanel secondaryPanel;
+    private JLabel basicLabel;
+    private JLabel tireLabel;
+    private JLabel errorLabel;
+    private JLabel miscLabel;
+    private JTable errorCodeTable;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
